eafd4f0d7d337fa509b8c8bbc663b2bdb0b5a58d
JoshuaWheaton <joshuawheaton25@gmail.com>
2019-08-11 21:18:19 -0500

    Add files via upload

---
+++ ConnectionManager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+const PORT = 1234
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func on_host_game():
+	pass
+
+func on_join_game(ip):
+	pass
